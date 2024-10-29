342616c7f056ffe7c698001f166b680d4a850ed8
thiagorocco <deroccotr@gmail.com>
2024-10-29 17:27:17 -0300

    alteração no arquivo 02-variaveis.php

--- 01-basico/02-variaveis.php
+++ 01-basico/02-variaveis.php
@@ -1,3 +1,14 @@
 <?php 
 $vetor = array('Palio','Gol','Fiesta','Corsa');
 var_dump($vetor);
+
+$a = 5;
+$b = &$a;
+$b = 10;
+echo($a);
+echo($b);
+
+$boolean = True;
+$inteiro = 34;
+$codigo_cliente = 144;
+
