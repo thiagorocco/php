d46b73150255c3ca0ef8c7bedeff6fb2a547e039
thiagorocco <deroccotr@gmail.com>
2024-11-01 07:08:35 -0300

    Exemplos de operadores aritméticos inseridos no arquivo 05-operadores-aritmeticos.php

--- 01-basico/05-operadores-aritmeticos.php
+++ 01-basico/05-operadores-aritmeticos.php
@@ -8,3 +8,19 @@
 
         Página 46
     */
+    $a = 30;
+    $b = 12;
+    $soma = $a+$b;
+    $subtracao = $a-$b;
+    $multi = $a*$b;
+    $divi = $a/$b;
+    $resto = $a%$b;
+    
+    echo 'Valor de a = '.$a;
+    echo '<br>Valor de b = '.$b;
+    echo '<br>Soma = '.$soma;
+    echo '<br>Subtração = '.$subtracao;
+    echo '<br>Multiplicação = '.$multi;
+    echo '<br>Divisão = '.$divi;
+    echo '<br>Resto da divisão = '.$resto;
+    
