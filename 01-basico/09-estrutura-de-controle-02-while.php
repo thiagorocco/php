ad53783e5f46eba6ac2de3d77764af60900b05ea
thiagorocco <deroccotr@gmail.com>
2024-11-06 13:20:25 -0300

    Adicionado o arquivo 09-estrutura-de-controle-02-while.php

---
+++ 01-basico/09-estrutura-de-controle-02-while.php
@@ -0,0 +1,10 @@
+<?php 
+
+    /*
+        Esse comando pode ser interpretado como “ENQUANTO (expressão) FAÇA {comandos... }.”.
+    */
+    $a = 1;
+    while($a <5){
+        print $a;
+        $a++;
+    }
