cd1c44cd110825382d7e72f086985934db858dd7
thiagorocco <deroccotr@gmail.com>
2024-11-07 13:35:16 -0300

    Adicionado arquivo 12-estrutura-de-controle-05-foreach.php

---
+++ 01-basico/12-estrutura-de-controle-05-foreach.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * O foreach é um laço de repetição para iterações em arrays ou matrizes.
+ */
+    $fruta = array('maçã','laranja','pera','banana');
+    foreach($fruta as $valor){
+        print "$valor - "; 
+    }
