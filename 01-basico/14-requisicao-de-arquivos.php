c920f2f4e46a2023ef7e08eab1dd9b644167cd25
thiagorocco <deroccotr@gmail.com>
2024-11-08 17:30:23 -0300

    update

---
+++ 01-basico/14-requisicao-de-arquivos.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *  Página 58
+ * 
+ *  Ambos incluem um arquivo externo, porém em caso de erro na requisição do arquivo externo: 
+ * 
+ *  include: Dá uma mensagem de warning
+ *  require: Dá um erro fatal
+ *  
+ */
