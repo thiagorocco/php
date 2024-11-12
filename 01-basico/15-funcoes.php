3e0f5ea9b5b2919287ad85e705a9580ed677ba8b
thiagorocco <deroccotr@gmail.com>
2024-11-12 12:53:09 -0300

    variavel estática, passagem por referência e valor do paramentro iniciado

--- 01-basico/15-funcoes.php
+++ 01-basico/15-funcoes.php
@@ -8,10 +8,32 @@
         return $valor;
     }
 
-    function calcula_imc($peso, $altura){
+    function calcula_imc($altura){
+        global $peso;
         return $peso / ($altura * $altura);
     }
 
+    //variável estática
+    function percorre($quilometros){
+        static $total;
+        $total += $quilometros;
+        echo "percorreu mais $quilometros do total de $total<br>\n";
+    }
+
+    //Passagem de parametro por referência, altera o valor da variável passada por parâmetro
+    //O segundo parametro tem o valor iniciado em 20, o que fica facultativo informar o valor para o segundo parametro na chamada da função
+    function incrementa(&$variavel, $valor=20){
+        $variavel += $valor;
+    }
+    $a = 10;
+    incrementa($a);//Não informado o segundo parâmetro
+    echo $a; //30
+
+    echo '<br>';    
     echo nome_da_funcao(1,2,3);
     echo '<br>';
-    echo calcula_imc(86,1.86);
+    echo calcula_imc(1.86);
+    echo '<br>';
+    percorre(100);
+    percorre(200);
+    percorre(50);
