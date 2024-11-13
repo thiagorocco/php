a84fd3fdaa7fcfd3790df202df15efaf7ac8c1a7
thiagorocco <deroccotr@gmail.com>
2024-11-13 12:58:17 -0300

    função recursiva

--- 01-basico/17-funcao-recursiva.php
+++ 01-basico/17-funcao-recursiva.php
@@ -1,3 +1,15 @@
 <?php 
 
     //função recursiva = função que retorna a própria função
+    function fatorial($numero){
+        if($numero ==1)
+            return $numero;
+        else
+            return $numero * fatorial($numero -1);
+    }
+    echo fatorial(6)."<br>\n";
+
+    /*
+        Na implementação acima, toda vez que retornar a própria função, o programa torna-se uma espécie de loop
+        e só vai parar quando encontrar uma condição que retorne algo que não seja a própria função
+    */
