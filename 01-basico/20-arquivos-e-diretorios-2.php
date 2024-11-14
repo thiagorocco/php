6c015e79ec086238ca9e6debf33a10ade8ee79e8
thiagorocco <deroccotr@gmail.com>
2024-11-14 17:42:14 -0300

    atualizar

--- 01-basico/20-arquivos-e-diretorios-2.php
+++ 01-basico/20-arquivos-e-diretorios-2.php
@@ -2,6 +2,7 @@
 
     //Escrevendo em um arquivo
     $fp = fopen ("file.txt", "w"); // abre o arquivo para gravação
+
     // escreve no arquivo
     fwrite ($fp, "linha 1" . PHP_EOL);
     fwrite ($fp, "linha 2" . PHP_EOL);
